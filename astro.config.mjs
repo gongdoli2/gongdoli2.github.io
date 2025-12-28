35fd679a85749a21ae9667f0b7569c3bc180f78e
Jimin Seo <aa090402@naver.com>
2025-12-28 14:25:34 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://gongdoli2.github.io/",
-    // output: "server"
+    output: "server"
 });
