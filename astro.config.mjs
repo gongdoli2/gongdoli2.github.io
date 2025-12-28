5ef3acd82885e7a649537facfcb519d18c4b9d8e
Jimin Seo <aa090402@naver.com>
2025-12-28 14:16:46 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
-    site: "https://gongdoli2.github.io/"
+  site: "https://gongdoli2.github.io/",
+  adapter: netlify()
 });
